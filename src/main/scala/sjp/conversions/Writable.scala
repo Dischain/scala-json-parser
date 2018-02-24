f50201bf0b41f5330680263b837177a62514a71f
Aleksands Shaytanov <rdischain@yandex.ru>
2018-02-24 23:57:17 +0400

    update docs

--- src/main/scala/sjp/conversions/Writable.scala
+++ src/main/scala/sjp/conversions/Writable.scala
@@ -3,29 +3,38 @@ package sjp.conversions
 import sjp.json.JsonVal
 
 /**
+  * Serializes an object to Json. You should define your own
+  * implicit `Writable` as shown in example below.
+  *
   * {{{
   * case class Person(name: String, address: Address)
   * case class Address(street: String, city: String)
   *
-  * implicit val addressWriteable = new Writable[Address] {
+  * implicit val addressWritable = new Writable[Address] {
   *   def write(addr: Address): JsonVal = {
   *     Json.obj(
-  *       street -> addr.street,
-  *       city -> addr.city
+  *       "street" -> JsonString(addr.street),
+  *       "city" -> JsonString(addr.city)
   *     )
   *   }
   * }
   *
-  * implicit val personWriteable = new Writable[Person] {
+  * implicit val personWritable = new Writable[Person] {
   *   def write(person: Person): JsonVal = {
   *     Json.obj(
-  *       name -> person.name,
-  *       address -> Json.toJson(person.address)
+  *       "name" -> JsonString(person.name),
+  *       "address" -> Json.toJson(person.address)
   *     )
   *   }
   * }
   * }}}
   */
 trait Writable[A] {
+  /**
+    * Convert an object into a JsonVal
+    *
+    * @param obj object to serialize
+    * @return JsonVal
+    */
   def write(obj: A): JsonVal
 }
