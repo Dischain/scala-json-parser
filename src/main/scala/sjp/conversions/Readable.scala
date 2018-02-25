b670a0443ef41847fc48ef16a1b6d24dd9a96614
Aleksands Shaytanov <rdischain@yandex.ru>
2018-02-26 01:56:24 +0400

    update scaladoc

--- src/main/scala/sjp/conversions/Readable.scala
+++ src/main/scala/sjp/conversions/Readable.scala
@@ -3,29 +3,37 @@ package sjp.conversions
 import sjp.json.JsonVal
 
 /**
+  * Decodes JsonVal instance into a optional value. Should be provided
+  * as implicit value, as shown in the example below.
+  *
   * {{{
   * case class Person(name: String, address: Address)
   * case class Address(street: String, city: String)
   *
   * implicit val addressRead = new Readable[Address] {
-  *   def read(addr: JsonVal): Option[Address] = {
+  *   def read(addr: JsonVal): Option[Person] = {
   *     for {
-  *       street <- (addr </> "street") as[String]
-  *       city <- (addr </> "city") as[String]
-  *     } Option(Address(street, city))
+  *       street <- addr </> "street"
+  *       city <- addr </> "city"
+  *     } yield Address(street.asString, city.asString)
   *   }
   * }
   *
   * implicit val personRead = new Readable[Person] {
   *   def read(person: JsonVal): Option[Person] = {
   *     for {
-  *       name <- (person </> "name") as[String]
-  *       address <- (person </> "address") as[Address]
-  *     }
+  *       name <- person </> "name"
+  *       address <- person </> "address"
+  *     } yield Person(name.asString, address.as[Address])
   *   }
   * }
   * }}}
   */
 trait Readable[A] {
+  /**
+    *
+    * @param jsonVal json value to be decoded
+    * @return optional value to be decoded
+    */
   def read(jsonVal: JsonVal): Option[A]
 }
--- src/main/scala/sjp/json/Json.scala
+++ src/main/scala/sjp/json/Json.scala
@@ -3,16 +3,54 @@ package sjp.json
 import sjp.conversions.{ Writable, Readable }
 import sjp.parser.Parser
 
+/**
+  * Set of helper functions to serialize/deserialize json values
+  */
 object Json {
+  /**
+    * Represent a [[sjp.json.JsonVal]] as json string
+    *
+    * @param value `JsonVal` to be stringified
+    * @return a string representation of `JsonVal`
+    */
   def stringify(value: JsonVal): String = value.stringify
 
+  /**
+    * Parse a json string and returns it as [[sjp.json.JsonVal]]
+    *
+    * @param string the string to parse
+    * @return `JsonVal`
+    */
   def parse(string: String): JsonVal = Parser.parse(string)
 
+  /**
+    * Convert an object to [[sjp.json.JsonVal]]
+    *
+    * @param obj object to be converted as `JsonVal`
+    * @param jw implicit [[sjp.conversions.Writable]] to serialize `obj`
+    *           as json object
+    * @tparam T the type of the value to be written as `JsonVal`
+    * @return json representation of an `obj`
+    */
   def toJson[T](obj: T)(implicit jw: Writable[T]): JsonVal =
     jw.write(obj)
 
+  /**
+    * Convert [[sjp.json.JsonVal]] to a value of requested type `T`
+    *
+    * @param value a json representation of an instance object of type `T`
+    * @param jr implicit [[sjp.conversions.Readable]] to deserialize `value`
+    * @tparam T the type of the value to be deserialized from `JsonVal`
+    * @return optional of expected type `T` representing required instance
+    */
   def fromJson[T](value: JsonVal)(implicit jr: Readable[T]): Option[T] =
     jr.read(value)
 
+  /**
+    * Creates a [[sjp.json.JsonObject]] from given `Seq` of key-value pairs
+    *
+    * @param members sequence of key-value pairs
+    * @return `JsonObject` constructed from the given pairs
+    */
   def obj(members: (String, JsonVal)*): JsonObject = JsonObject(members)
 }
